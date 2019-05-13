85ced08683b82fe8f16e0adf260e008401358431
João Pedro dos Santos Rosa <joao.rosa.a7@ea.org.br>
2019-05-13 20:37:59 -0300

    Adicionando commit inicial

---
+++ src/com/entities/User.java
@@ -0,0 +1,31 @@
+package com.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+	private String name;
+	private String img;
+	private String email;
+	private String pswd;
+	private List<User> friends = new ArrayList<>();
+	
+	
+	public User(String name, String email, String pswd){
+		this.name = name;
+		this.email = email;
+		this.pswd = pswd;
+	}
+	
+	public void setImage(String img) {
+		this.img = img;
+	}
+	
+	public void addFriend(User user) {
+		this.friends.add(user);
+	}
+	
+	public void deleteFriend(User user) {
+		this.friends.remove(user);
+	}
+}
---
+++ src/com/entities/schedule.java
@@ -0,0 +1,7 @@
+package com.entities;
+
+import java.util.Date;
+
+public class schedule {
+	public Date periodo;
+}
